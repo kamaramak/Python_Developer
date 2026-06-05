7de9febb3f7f0b142db2af84ec759a37d665df0a
Marat Kabardokov <kabardokov.m@yandex.ru>
2026-06-05 13:40:53 +0300

    Delete Cat charity directory

--- Cat charity/app/models/user.py
+++
@@ -1,9 +0,0 @@
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
-from sqlalchemy import Integer
-from sqlalchemy.orm import Mapped, mapped_column
-
-from app.core.db import Base
-
-
-class User(SQLAlchemyBaseUserTable[int], Base):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
